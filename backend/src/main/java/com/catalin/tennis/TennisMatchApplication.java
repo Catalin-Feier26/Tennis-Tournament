a18626f0374d7fcebb0296003177710d1e8dcbc8
Catalin <feiercatalin123@icloud.com>
2025-03-28 00:53:33 +0200

    Initial commit, project setup

---
+++ backend/src/main/java/com/catalin/tennis/TennisMatchApplication.java
@@ -0,0 +1,13 @@
+package com.catalin.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisMatchApplication.class, args);
+    }
+
+}
